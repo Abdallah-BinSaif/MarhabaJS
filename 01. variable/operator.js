43923653df1ce6dc5acb3b18bc75f9ffad4525cb
Abdallah-BinSaif <binsaif.info@gmail.com>
2025-04-19 20:29:50 +0600

    operator practice

---
+++ 01. variable/operator.js
@@ -0,0 +1,52 @@
+const income = 100;
+const expence = 40;
+const due = income -expence;
+console.log(due);
+
+
+const totalPencil = 10;
+const usedPencil = 2;
+const totalUse = totalPencil/usedPencil;
+console.log(totalUse);
+
+
+const hasMoney = 60;
+const perExpence = 15;
+const expenceCount = hasMoney/perExpence;
+const dueMoney = hasMoney % perExpence;
+console.log(expenceCount, dueMoney);
+
+
+const str1 = "hello";
+const str2 = "World";
+const concate = str1 + str2;
+console.log(concate);
+
+const rice = 153;
+const dall = 261;
+const totalWeight = rice + dall;
+console.log(totalWeight);
+
+
+const againHasMoney = 500;
+const againPerExpence = 75;
+const againExpenceCount = againHasMoney / againPerExpence;
+const againDueMoney = againHasMoney % againPerExpence;
+console.log(againExpenceCount, againDueMoney);
+
+console.log(8 / 3, 8 % 3)
+
+console.log(50 % 9)
+
+const bangla = "Bangla";
+const desh = "desh";
+const bangladesh = bangla + desh;
+console.log(bangladesh);
+
+
+
+const totalRice = 480;
+const dailyUsageRice = 4 ;
+const totalDay = totalRice / dailyUsageRice;
+const totalMonth = totalDay / 30;
+console.log(totalDay, totalMonth);
