29f6e0a19b126dca267186fc5e3f122e809f2196
Abdallah-BinSaif <binsaif.info@gmail.com>
2025-04-19 18:23:43 +0600

    variable and console practice done

---
+++ 01. variable/console.js
@@ -0,0 +1,24 @@
+const age = 26;
+console.log(age);
+
+const booksToRead = 2;
+console.log(booksToRead);
+
+const wakeUpTime = 7;
+console.log(wakeUpTime);
+
+const wastedTime = 5;
+console.log(wastedTime);
+
+const borrow = 0;
+console.log(borrow);
+
+const goals = 3;
+console.log(goals);
+
+const pillow = 3;
+console.log(pillow);
+
+
+
+
