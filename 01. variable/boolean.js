a647cf98945641ad567a3f0c2826e6a9b58b460f
Abdallah-BinSaif <binsaif.info@gmail.com>
2025-04-19 19:44:34 +0600

    boolean practive

---
+++ 01. variable/boolean.js
@@ -0,0 +1,20 @@
+const bamboo = true;
+console.log(bamboo);
+
+const crush = false;
+console.log(crush);
+
+const celebrating = true;
+console.log(celebrating);
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy, hasMoney, isHungry);
+
+const isPhoneCharged = false;
+console.log(isPhoneCharged);
+
+const isDinnerDone = false;
+console.log(isDinnerDone);
+
