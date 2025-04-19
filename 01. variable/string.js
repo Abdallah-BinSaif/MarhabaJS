3867e871f2ffc7d51cad831c8c6653d33f0e8e64
Abdallah-BinSaif <binsaif.info@gmail.com>
2025-04-19 19:38:37 +0600

    string practive

---
+++ 01. variable/string.js
@@ -0,0 +1,20 @@
+const name = "abdallah bin saif";
+console.log(name);
+
+const address = "cumilla, dhaka";
+console.log(address);
+
+const school = "north south univercity"
+console.log(school)
+
+const fan = "Mohammad <s>";
+console.log(fan);
+
+const movieOfTheYear = "Marvel";
+console.log(movieOfTheYear);
+
+const hobby = "coding";
+console.log(hobby);
+
+const color = "green";
+console.log(color);
