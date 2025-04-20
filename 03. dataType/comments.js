c678915e0c2edba492e570672de49e2a042a17ba
Abdallah-BinSaif <binsaif.info@gmail.com>
2025-04-20 23:31:49 +0600

    comments practice

---
+++ 03. dataType/comments.js
@@ -0,0 +1,42 @@
+// this is exam score
+const score = 81;
+
+//2 this is circle radius
+const radius = 5;
+
+
+// 3. 
+/**
+ * 2 -> pants
+ * 3 -> shirts
+ * 3 -> Tshirt
+ */
+const dress = 8;
+
+// 4.
+//Eid bonus
+let giftMoney = 100;
+
+
+//5.
+/* 
+1=> bangla
+1=> english
+1.5=> chemestri
+1.5=> physics
+2=> mathmatics
+*/
+let studyTime = 7 
+
+
+//6.
+/*
+Name="Rashed"
+Amount=5000
+service charge=25
+*/
+let sendMoney =5000;
+
+// 7.
+// product price and delivery charge included
+let pay = 777;
--- 03. dataType/letConst.js
+++ 03. dataType/letConst.js
@@ -10,3 +10,21 @@ console.log(pocketMoney);
 let studyTime = 4;
 studyTime = 0;
 console.log(studyTime);
+
+let friendsCount = 3;
+friendsCount = 2;
+console.log(friendsCount);
+
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+let chairsCount = 40;
+chairsCount = 38;
+chairsCount = 32;
+console.log(chairsCount);
+
+
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
