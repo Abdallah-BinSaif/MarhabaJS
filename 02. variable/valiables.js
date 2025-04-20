b99a6b9c64148cffcc6a49ba74fb4f62aaab368e
Abdallah-BinSaif <binsaif.info@gmail.com>
2025-04-20 21:52:39 +0600

    typeof and letConst done

---
+++ 02. variable/valiables.js
@@ -0,0 +1,15 @@
+const birthYear = 1990;
+const brothers = 2;
+const sisters = 3;
+
+const students = 23;
+
+const marks = 66;
+
+const rooms = 4;
+
+const totalReadTime = 4;
+
+const total = 11;
+
+
---
+++ 03. dataType/letConst.js
@@ -0,0 +1,12 @@
+let age = 26;
+console.log(age);
+age = 29;
+console.log(age);
+
+let pocketMoney = 500;
+pocketMoney= 150;
+console.log(pocketMoney);
+
+let studyTime = 4;
+studyTime = 0;
+console.log(studyTime);
